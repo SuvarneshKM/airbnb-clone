e03d04a26fd0d71bcf24fb13c38d43656f351db2
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-07 22:22:21 +0530

    Building the Left Side of the Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,16 @@
+import Image from 'next/image'
+
 function Header() {
     return (
-        <header>
-            <h1>Header</h1>
-
+        <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* left */}
-            <div></div>
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+                <Image src="https://links.papareact.com/qd3" 
+                layout='fill' 
+                objectFit='contain'
+                objectPosition='left'
+                />
+            </div>
 
             {/* middle */}
             <div></div>
