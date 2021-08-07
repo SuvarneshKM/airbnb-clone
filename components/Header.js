9969affb5119537c3c09060dc90c81c2b80a1257
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-07 14:40:10 +0530

    Building the Header Component

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <header>
+            <h1>Header</h1>
+
+            {/* left */}
+            <div></div>
+
+            {/* middle */}
+            <div></div>
+
+            {/* right */}
+            <div></div>
+        </header>
+    )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Airbnb Clone 🚀 </h1>
+      <Header />
+      {/* Banner */}
+
     </div>
   )
 }
