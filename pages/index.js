a87262485b9fa4d194a458a7049f682fbb75efd4
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-08 17:55:30 +0530

    main section started

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,15 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5 ">
+            Explore Nearby
+          </h2>
+          {/* Pull some data from a server - API endpoints */}
+        </section>
+      </main>
     </div>
   )
 }
