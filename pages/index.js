cdef2cb692f53ab84318a347859a930420e16757
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-08 16:52:07 +0530

    hero section-banner

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 export default function Home() {
@@ -9,8 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* Banner */}
-
+      <Banner />
     </div>
   )
 }
