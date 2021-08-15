736a0dd635b18c1f2d47effa07ab3752a857bdba
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-15 19:08:29 +0530

    Medium Card Done 🚀

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -33,6 +34,15 @@ export default function Home({ exploreData }) {
         </section>
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className=" flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard
+                key={img}
+                img={img}
+                title={title}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </div>
@@ -44,9 +54,13 @@ export async function getStaticProps() {
     .then(res => res.json()
     );
 
+  const cardsData = await fetch('https://links.papareact.com/zp1')
+    .then(res => res.json()
+    );
   return {
     props: {
-      exploreData
+      exploreData,
+      cardsData
     }
   }
 }
