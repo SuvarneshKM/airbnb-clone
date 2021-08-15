d69faad3c22b61bd9fb47b610897504889c8b159
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-15 20:04:56 +0530

    Footer Done 🚀

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer';
 import Header from '../components/Header'
 import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
@@ -52,6 +53,7 @@ export default function Home({ exploreData, cardsData }) {
           buttonText="Get Inspired"
         />
       </main>
+      <Footer />
     </div>
   )
 }
