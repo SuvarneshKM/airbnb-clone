7fe4350c98f36d259550e737e4e8b3b1292b6d35
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-18 01:25:31 +0530

    useState

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,11 @@ import {
     UserCircleIcon,
     UsersIcon,
 } from '@heroicons/react/solid'
+import { useState } from 'react'
 
 function Header() {
+    const[searchInput,setSearchInput] = useState("");
+
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* left */}
@@ -21,7 +24,13 @@ function Header() {
 
             {/* middle */}
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
+                <input 
+                value={searchInput} 
+                onChange={(e)=> setSearchInput(e.target.value)}
+                 className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" 
+                 type="text" 
+                 placeholder="Start your search"
+                  />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
 
