7c55ca564fcc0ecddff69d9a74ccd17491935152
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-20 20:54:58 +0530

    search page

--- components/Header.js
+++ components/Header.js
@@ -10,23 +10,36 @@ import { useState } from 'react'
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
+import { useRouter } from 'next/dist/client/router';
 
 
-function Header() {
+function Header({ placeholder }) {
     const [searchInput, setSearchInput] = useState("");
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [noOfGuests, setNoOfGuests] = useState(1);
+    const router = useRouter();
 
     const handleSelect = (ranges) => {
         setStartDate(ranges.selection.startDate);
         setEndDate(ranges.selection.endDate);
     }
 
-    const resetInput =() => {
+    const resetInput = () => {
         setSearchInput("");
     }
 
+    const search = () => {
+        router.push({
+            pathname: '/search',
+            query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+                noOfGuests,
+            }
+        });
+    }
     const selectionRange = {
         startDate: startDate,
         endDate: endDate,
@@ -37,7 +50,7 @@ function Header() {
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* left */}
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div onClick={() => { router.push("/") }} className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image src="https://links.papareact.com/qd3"
                     layout='fill'
                     objectFit='contain'
@@ -52,7 +65,7 @@ function Header() {
                     onChange={(e) => setSearchInput(e.target.value)}
                     className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
                     type="text"
-                    placeholder="Start your search"
+                    placeholder={placeholder || "Start your search"}
                 />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
@@ -77,18 +90,18 @@ function Header() {
                     />
                     <div className="flex items-center border-b mb-4">
                         <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
-                        <UsersIcon className="h-5"/>
+                        <UsersIcon className="h-5" />
                         <input
-                        value={noOfGuests} 
-                        onChange={(e) => setNoOfGuests(e.target.value)}
-                        type="number" 
-                        min={1}
-                        className="w-12 pl-2 text-lg outline-none text-red-400" 
+                            value={noOfGuests}
+                            onChange={(e) => setNoOfGuests(e.target.value)}
+                            type="number"
+                            min={1}
+                            className="w-12 pl-2 text-lg outline-none text-red-400"
                         />
                     </div>
                     <div className="flex">
                         <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
-                        <button className="flex-grow text-red-400">Search</button>
+                        <button onClick={search} className="flex-grow text-red-400">Search</button>
                     </div>
                 </div>
             }
--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,34 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useRouter } from "next/dist/client/router";
+import { format } from "date-fns";
+
 function Search() {
+    const router = useRouter();
+    const { location, startDate, endDate, noOfGuest } = router.query;
+
+    const formatedStartDate = format(new Date(startDate), "dd MMMM yy");
+    const formatedEndDate = format(new Date(endDate), "dd MMMM yy");
+    const range = `${formatedStartDate} - ${formatedEndDate}`;
+
     return (
         <div>
-            <h1>I am the search page</h1>
+            <Header placeholder={`${location} | ${range} | ${noOfGuest} guests`} />
+
+            <main className="flex">
+                <section className="flex-grow pt-14 px-6 ">
+                    <p className="text-xs ">300+ Stays - {range} - for {noOfGuest} guests</p>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in { location }</h1>
+                    <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                        <p className="button">Cancellation Flexibility</p>
+                        <p className="button">Type of Place</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms and Beds</p>
+                        <p className="button">More filters</p>
+                    </div>
+                </section>
+            </main>
+            <Footer />
         </div>
     )
 }
