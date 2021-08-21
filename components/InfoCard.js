fcca962a53b67a334b72dce6ff54b067c31c2259
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-21 17:12:12 +0530

    infocard

---
+++ components/InfoCard.js
@@ -0,0 +1,9 @@
+function InfoCard({ img, location, title, description, star, price, total }) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default InfoCard
--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,9 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useRouter } from "next/dist/client/router";
 import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
 
-function Search() {
+function Search({ searchResults }) {
     const router = useRouter();
     const { location, startDate, endDate, noOfGuest } = router.query;
 
@@ -18,13 +19,28 @@ function Search() {
             <main className="flex">
                 <section className="flex-grow pt-14 px-6 ">
                     <p className="text-xs ">300+ Stays - {range} - for {noOfGuest} guests</p>
-                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in { location }</h1>
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
                     <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                         <p className="button">Cancellation Flexibility</p>
                         <p className="button">Type of Place</p>
                         <p className="button">Price</p>
                         <p className="button">Rooms and Beds</p>
                         <p className="button">More filters</p>
+                    </div>
+
+                    <div className="flex flex-col">
+                    {searchResults.map(({ img, location, title, description, star, price, total }) => (
+                        <InfoCard
+                            key={img}
+                            img={img}
+                            location={location}
+                            title={title}
+                            description={description}
+                            star={star}
+                            price={price}
+                            total={total}
+                        />
+                    ))}
                     </div>
                 </section>
             </main>
@@ -34,3 +50,14 @@ function Search() {
 }
 
 export default Search
+
+export async function getServerSideProps() {
+    const searchResults = await fetch("https://links.papareact.com/isz")
+        .then(res => res.json());
+
+    return {
+        props: {
+            searchResults,
+        }
+    }
+}
