a6a10ce9cac95c0ec901ea8997e1b09afae7d8bf
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-21 21:25:18 +0530

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,37 +2,48 @@ function Footer() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32
         py-14 bg-gray-100 text-gray-600">
-            <div className="space-y-4 text-xs text-gray-800">
-                <h5 className="font-bold">ABOUT</h5>
+            <div className="space-y-4 text-xs text-gray-800 ">
+                <div className="border-t border-gray-300  pt-2" />
+                <h5 className="font-bold ">ABOUT</h5>
                 <p>How Airbnb works</p>
                 <p>Newsroom</p>
-                <p>Investors</p>
+                <p>Airbnb 2021</p>
                 <p>Airbnb Plus</p>
                 <p>Airbnb Luxe</p>
+                <p>HotelTonight</p>
+                <p>Airbnb for Work</p>
+                <p>Made possible by Hosts</p>
+                <p>Careers</p>
+                <p>Founders' Letter</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
+                <div className="border-t border-gray-300  pt-2" />
                 <h5 className="font-bold">COMMUNITY</h5>
+                <p>Diversity & Belonging</p>
                 <p>Accessibility</p>
-                <p>This is not a real site</p>
-                <p>Its a pretty awesome clone</p>
-                <p>Referrals accepted</p>
-                <p>Papafam</p>
+                <p>Airbnb Associates</p>
+                <p>Frontline Stays</p>
+                <p>Guest Referrals</p>
+                <p>Airbnb.org</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
+                <div className="border-t border-gray-300  pt-2" />
                 <h5 className="font-bold">HOST</h5>
-                <p>Papa React</p>
-                <p>Presents</p>
-                <p>Zero to Full Stack Hero</p>
-                <p>Hundreds of Students</p>
-                <p>Join Now</p>
+                <p>Host your home</p>
+                <p>Host an Online Experience</p>
+                <p>Host an Experience</p>
+                <p>Responsible hosting</p>
+                <p>Resource Centre</p>
+                <p>Community Centre</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
+                <div className="border-t border-gray-300  pt-2" />
                 <h5 className="font-bold">SUPPORT</h5>
+                <p>Our COVID-19 Response</p>
                 <p>Help Centre</p>
+                <p>Cancellation options</p>
+                <p>Neighbourhood Support</p>
                 <p>Trust & Safety</p>
-                <p>Say Hi YouTube</p>
-                <p>Easter Eggs</p>
-                <p>For the Win</p>
             </div>
         </div>
     )
