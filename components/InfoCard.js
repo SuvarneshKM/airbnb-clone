eaf056fcde668e5ad3496ba09c8a7b3f02e6676c
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-21 18:47:52 +0530

    loading..

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -9,6 +9,7 @@ function InfoCard({ img, location, title, description, star, price, total }) {
                 src={img} 
                 layout="fill" 
                 objectFit="cover"
+                className="rounded-2xl"
                 />
             </div>
             <div className="flex flex-col flex-grow pl-5">
